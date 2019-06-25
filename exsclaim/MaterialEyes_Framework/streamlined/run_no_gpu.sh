7c0ed68560ca8ee2383c430a094e57406534b200
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-06-25 13:50:01 +0000

    streamlined automation bug fix

--- exsclaim/MaterialEyes_Framework/streamlined/run_no_gpu.sh
+++ exsclaim/MaterialEyes_Framework/streamlined/run_no_gpu.sh
@@ -3,6 +3,6 @@ gdown https://drive.google.com/uc?id=1xWxqQGDH_szfCe8eWDBwTcjzCmq7Bnf1
 unzip snapshot930.ckpt.zip
 mkdir detection_compound_figure/checkpoints
 mv snapshot930.ckpt detection_compound_figure/checkpoints
-cd detection_compound_figures
+cd detection_compound_figure
 ## write desired image directory in here
 python test.py --detect_thresh 0.5 --ckpt checkpoints/snapshot930.ckpt --image_dir ./sample_input/ --result_dir ./sample_results/ --image_extend png --gpu -1
