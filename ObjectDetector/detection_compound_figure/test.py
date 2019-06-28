293d4bfb6ad43eb63bd89e11dc4fe9b9d681a17b
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-06-28 15:08:57 -0500

    Uncommented cropping portion

--- ObjectDetector/detection_compound_figure/test.py
+++ ObjectDetector/detection_compound_figure/test.py
@@ -16,11 +16,9 @@ from dataset.figsepdataset import *
 
 import time
 
-def main():
-    """
-    Visualize the detection result for the given image and the pre-trained model.
-    """
-    parser = argparse.ArgumentParser()
+def parse_arguments():
+	""" reads command line arguments and returns them in NameSpace object """
+	parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--cfg', type=str, default='config/yolov3_default.cfg')
     parser.add_argument('--ckpt', type=str,
@@ -35,11 +33,86 @@ def main():
     parser.add_argument('--gt_anno_dir', type=str)
     parser.add_argument('--max_img', type=int,default=2000)
     parser.add_argument('--background', action='store_true',
-                        default=False, help='background(no-display mode. save "./output.png")')
+                        default=False, 
+						help='background(no-display mode. save "./output.png")')
     parser.add_argument('--detect_thresh', type=float,
                         default=None, help='confidence threshold')
     parser.add_argument('--result_dir', type=str)
     args = parser.parse_args()
+	return args
+	
+def load_model(args):
+    if args.groundtruth:
+        args.cfg = 'config/yolov3_eval.cfg'
+        with open(args.cfg, 'r') as f:
+            cfg = yaml.load(f)
+
+        imgsize = cfg['TEST']['IMGSIZE']
+        model = YOLOv3(cfg['MODEL'])
+
+        confthre = cfg['TEST']['CONFTHRE'] 
+        nmsthre = cfg['TEST']['NMSTHRE']
+        batch_size = cfg['TRAIN']['BATCHSIZE']
+        
+        if args.detect_thresh:
+            confthre = args.detect_thresh
+            
+        dataset = FigSepDataset(model_type=cfg['MODEL']['TYPE'],
+                              data_dir=args.gt_data_dir,
+                              anno_dir = args.gt_anno_dir,
+                              img_size=cfg['TRAIN']['IMGSIZE'],
+                              augmentation=cfg['AUGMENTATION'],
+                              debug=False)
+        dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+        dataiterator = iter(dataloader)
+        
+        cuda = torch.cuda.is_available()
+        
+        if cuda:
+            print("using cuda") 
+            model = model.cuda()
+        
+        dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        
+        total_loss = 0
+        for i in range(args.max_img):
+            try:
+                imgs, targets, _, _ = next(dataiterator)
+            except StopIteration:
+                print("{} images has been assessed".format(i+1))
+                break
+            imgs = Variable(imgs.type(dtype))
+            targets = Variable(targets.type(dtype), requires_grad=False)
+            loss = model(imgs, targets)
+            total_loss += loss.data.cpu().numpy()
+        print("loss is {}".format(total_loss/(i+1)))
+        return
+
+    
+    
+    
+    
+    with open(args.cfg, 'r') as f:
+        cfg = yaml.load(f)
+
+    imgsize = cfg['TEST']['IMGSIZE']
+    model = YOLOv3(cfg['MODEL'])
+
+    confthre = cfg['TEST']['CONFTHRE'] 
+    nmsthre = cfg['TEST']['NMSTHRE']
+#     confthre = 0
+#     nmsthre = 1
+
+    if args.detect_thresh:
+        confthre = args.detect_thresh
+
+def main():
+    """
+    Visualize the detection result for the given image and the pre-trained model.
+    """
+    ## get arguments passed in through command line
+	args = parse_arguments()
 
     
     if args.groundtruth:
@@ -179,25 +252,25 @@ def main():
             confidences.append("%.3f"%(cls_conf.item()))
 
         
-#         from PIL import Image
-#         test_img = Image.open(args.image).convert("RGB")
-#         sub_figures = []
-#         for i in range(len(bboxes)):
-#             box = bboxes[i]
-#             h,w = test_img.size
-#             y0 = int(max(box[0].data.cpu().numpy(),0))
-#             x0 = int(max(box[1].data.cpu().numpy(),0))
-#             y1 = int(min(box[2].data.cpu().numpy(),w-1))
-#             x1 = int(min(box[3].data.cpu().numpy(),h-1))
-# #             print(x0,y0,x1,y1,type(x0))
-#             sub_figure = test_img.crop((x0,y0,x1,y1))
-#             if classes[i] == 1:
-#                 sub_figures.append(sub_figure)
-# #         if not os.path.exists(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0])):
-# #             os.makedirs(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]))
-#         for i in range(len(sub_figures)):
-#             sub_figures[i].save(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]+"----{}_output.png".format(i+1)))
-#             sub_figures[i].save(os.path.join(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]),"{}_output.png".format(i+1)))
+         from PIL import Image
+         test_img = Image.open(args.image).convert("RGB")
+         sub_figures = []
+         for i in range(len(bboxes)):
+             box = bboxes[i]
+             h,w = test_img.size
+             y0 = int(max(box[0].data.cpu().numpy(),0))
+             x0 = int(max(box[1].data.cpu().numpy(),0))
+             y1 = int(min(box[2].data.cpu().numpy(),w-1))
+             x1 = int(min(box[3].data.cpu().numpy(),h-1))
+             print(x0,y0,x1,y1,type(x0))
+             sub_figure = test_img.crop((x0,y0,x1,y1))
+             if classes[i] == 1:
+                 sub_figures.append(sub_figure)
+         if not os.path.exists(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0])):
+             os.makedirs(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]))
+         for i in range(len(sub_figures)):
+             sub_figures[i].save(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]+"----{}_output.png".format(i+1)))
+             sub_figures[i].save(os.path.join(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]),"{}_output.png".format(i+1)))
         
         
         from PIL import Image, ImageDraw, ImageFont
@@ -229,10 +302,10 @@ def main():
             text_width, text_height = font.getsize(text)
             draw.text((x0,y0),text,fill=rect_color,font=font)
         del draw
-#         if not os.path.exists(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0])):
-#             os.makedirs(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]))
+        if not os.path.exists(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0])):
+            os.makedirs(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]))
         test_img.save(os.path.join(args.result_dir,sample_img.split("/")[-1]))
-#         test_img.save(os.path.join(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]),"output.png"))
+        test_img.save(os.path.join(os.path.join(args.result_dir,sample_img.split("/")[-1].split(".")[0]),"output.png"))
     
     
 #     print("bbbox=",bboxes)
