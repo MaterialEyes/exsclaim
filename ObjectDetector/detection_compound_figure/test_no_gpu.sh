4303e376f08a35b58abfa0241fbd9896a15a2622
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-07-02 14:38:41 -0500

    Moved detection_compound_figure to utils/old/streamlined

--- ObjectDetector/detection_compound_figure/test_no_gpu.sh
+++
@@ -1,7 +0,0 @@
-## for multi image detection
-python3 test.py --detect_thresh 0.5 --ckpt checkpoints/snapshot930.ckpt --image_dir ./sample_input/ --result_dir ./sample_results/ --image_extend png --gpu -1
-
-## for single image detection
-#python3 test.py --detect_thresh 0.5 --ckpt checkpoints/snapshot930.ckpt --image ./sample_input/complex_1.png --result_dir ./sample_results/ --image_extend png --gpu -1
-
-
