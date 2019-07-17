44b598cf699dae0ee7cc7c025be80d9023092945
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-07-17 11:57:03 -0500

    Added global setup for use with conda

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+conda env create -f environment.yml
+conda activate exsclaim_env
+
+
+
+FILE=TextDetector/models/read_sflabel_5_CNN150_adam.pt
+if [ ! -f "$FILE" ]; then
+    mkdir TextDetector/models
+    gdown https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb TextDetector/models
+fi
