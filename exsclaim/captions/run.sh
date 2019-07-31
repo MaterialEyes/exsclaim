327e4c5dca2b8da92a8183faa2a43c71ec0bbbfc
Eric <e.schwenker89@gmail.com>
2019-07-31 00:27:49 -0500

    Replace run.py with assign.py which takes exsclaim.json and query.json as input after scraper

--- exsclaim/captions/run.sh
+++
@@ -1,3 +0,0 @@
-## for multi images detection
-python3 run.py  --images_dir ./dataset/images/ --image_extend gif --csv_path ./test/csv/key.csv  --stok ./config/sentence_search_patterns.yml --keyword "HAADF-STEM"  --synonymns "HAADF","HAADF-STEM","HAADF–STEM","High-angle annular dark-field","High angle annular dark-field","High-angle annular darkfield","High angle annular darkfield"
-
