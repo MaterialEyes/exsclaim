2a6cb15475dc939ff0a744043bc5a9e66d6523f3
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-08-09 20:00:05 +0000

    cleaned text folder

--- exsclaim/text/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/sh
-
-gdown https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
-mkdir models
-mv read_sflabel_5_CNN150_adam.pt models/read_sflabel_5_CNN150_adam.pt
--- exsclaim/text/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source .env/bin/activate
-pip install -r requirements.txt
-python train.py -a "CNN1" -g "adam" -b "50"
