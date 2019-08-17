6479d11de445a1e8bd70c3be41423c4013a401a1
Trevor Spreadbury <trevorspreadbury@gmail.com>
2019-08-17 22:43:41 +0000

    added maximal .yml file and new setup file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-conda env create -f environment.yml
-conda activate exsclaim_env
-
-
 
 text_model=exsclaim/text/models/read_sflabel_5_CNN150_adam.pt
 text_dir=exsclaim/text/models
@@ -10,6 +6,7 @@ if [ ! -f "$text_model" ]; then
     if [ ! -d "$text_dir" ]; then
         mkdir exsclaim/text/models
     fi
+    # Changle link below if a new model is trained
     gdown  -O exsclaim/text/models/read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
 else
     echo "Already downloaded TextDetector model"
@@ -21,6 +18,7 @@ if [ ! -f "$object_model" ]; then
     if [ ! -d "$object_dir" ] ; then
         mkdir exsclaim/objects/checkpoints
     fi
+    # Change link below if a new model is trained
     gdown -O exsclaim/objects/checkpoints/snapshot930.ckpt.zip https://drive.google.com/uc?id=1xWxqQGDH_szfCe8eWDBwTcjzCmq7Bnf1
     unzip exsclaim/objects/checkpoints/snapshot930.ckpt.zip -d exsclaim/objects/checkpoints
     rm exsclaim/objects/checkpoints/snapshot930.ckpt.zip
@@ -28,3 +26,4 @@ else
     echo "Already downloaded ObjectDetector model"
 fi
 
+python -m spacy download en_core_web_sm
