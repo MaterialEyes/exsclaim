a18e19d4d1e3a457465acdb81dee81d36eb5f2b8
eschwenk <e.schwenker89@gmail.com>
2019-10-28 17:15:39 -0500

    Fix bugs in setup.sh
    Fix bugs in setup.sh

--- exsclaim/imagetexts/models/download.sh
+++
@@ -1,1 +0,0 @@
-gdown  -O read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ esac
 USE=${1:-min}
 
 # Create conda environment 
-envfile="envs/"$OS"/"$USE".yml"
+envfile="conda-env/"$OS"/"$USE".yml"
 conda env create -f "$envfile";
 
 # Get the environment name from the yaml file
@@ -28,29 +28,29 @@ envname=$(grep "name: *" $envfile | sed -n -e 's/name: //p')
 source activate "$envname";
 
 # Download the text detector model
-text_model=exsclaim/text/models/read_sflabel_5_CNN150_adam.pt
-text_dir=exsclaim/text/models
+text_model=exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt
+text_dir=exsclaim/imagetexts/models
 if [ ! -f "$text_model" ]; then
     if [ ! -d "$text_dir" ]; then
-        mkdir exsclaim/text/models
+        mkdir exsclaim/imagetexts/models
     fi
     # Changle link below if a new model is trained
-    gdown  -O exsclaim/text/models/read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
+    gdown  -O exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
 else
     echo "Already downloaded TextDetector model"
 fi
 
 # Download the object detector model
-object_model=exsclaim/objects/checkpoints/snapshot930.ckpt
-object_dir=exsclaim/objects/checkpoints
+object_model=exsclaim/figures/models/checkpoints/snapshot930.ckpt
+object_dir=exsclaim/figures/models/checkpoints
 if [ ! -f "$object_model" ]; then
     if [ ! -d "$object_dir" ] ; then
-        mkdir exsclaim/objects/checkpoints
+        mkdir exsclaim/figures/models/checkpoints
     fi
     # Change link below if a new model is trained
-    gdown -O exsclaim/objects/checkpoints/snapshot930.ckpt.zip https://drive.google.com/uc?id=1xWxqQGDH_szfCe8eWDBwTcjzCmq7Bnf1
-    unzip exsclaim/objects/checkpoints/snapshot930.ckpt.zip -d exsclaim/objects/checkpoints
-    rm exsclaim/objects/checkpoints/snapshot930.ckpt.zip
+    gdown -O exsclaim/figures/models/checkpoints/snapshot930.ckpt.zip https://drive.google.com/uc?id=1xWxqQGDH_szfCe8eWDBwTcjzCmq7Bnf1
+    unzip exsclaim/figures/models/checkpoints/snapshot930.ckpt.zip -d exsclaim/figures/models/checkpoints
+    rm exsclaim/figures/models/checkpoints/snapshot930.ckpt.zip
 else
     echo "Already downloaded ObjectDetector model"
 fi
