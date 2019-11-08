8f6fd5fb5ed443eb2925c7e875ce59248a0a2c08
eschwenk <e.schwenker89@gmail.com>
2019-11-08 15:01:35 -0600

    Remove Text Reader download script

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -1,15 +1,15 @@
-# Download the text detector model
-text_model=exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt
-text_dir=exsclaim/imagetexts/models
-if [ ! -f "$text_model" ]; then
-    if [ ! -d "$text_dir" ]; then
-        mkdir exsclaim/imagetexts/models
-    fi
-    # Changle link below if a new model is trained
-    gdown  -O exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
-else
-    echo "Already downloaded TextDetector model"
-fi
+# # Download the text detector model
+# text_model=exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt
+# text_dir=exsclaim/imagetexts/models
+# if [ ! -f "$text_model" ]; then
+#     if [ ! -d "$text_dir" ]; then
+#         mkdir exsclaim/imagetexts/models
+#     fi
+#     # Changle link below if a new model is trained
+#     gdown  -O exsclaim/imagetexts/models/read_sflabel_5_CNN150_adam.pt https://drive.google.com/uc?id=1pkBWn0Ss0c9TeAgsOBFqFFJZ-5ntxzpb
+# else
+#     echo "Already downloaded TextDetector model"
+# fi
 
 # Download the object detector model
 object_model=exsclaim/figures/models/checkpoints/snapshot20000.ckpt
@@ -25,7 +25,3 @@ if [ ! -f "$object_model" ]; then
 else
     echo "Already downloaded ObjectDetector model"
 fi
-
-
-# Download the English spaCy model
-# python -m spacy download en_core_web_sm
