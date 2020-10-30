646fb67550f610f04d795c9c0335cd443eee3a4a
Trevor Spreadbury <trevorspreadbury@gmail.com>
2020-10-30 11:26:52 -0500

    update setup.py to include dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here / "README.md", "r") as fh:
 
 setuptools.setup(
     name="exsclaim-tspread",
-    version="0.0.4",
+    version="0.0.12",
     author=('Eric Schwenker','Trevor Spreadbury','Weixin Jiang','Maria Chan'),
     author_email="developers@materialeyes.org",
     description="EXSCLAIM! is a library for the automatic EXtraction, Separation, and Caption-based natural Language Annotation of IMages from scientific figures.",
@@ -16,13 +16,29 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MaterialEyes/exsclaim",
     packages=setuptools.find_packages(),
+    install_requires= [
+        "numpy",
+        "pillow",
+        "beautifulsoup4",
+        "pyyaml",
+        "scipy",
+        "scikit-image",
+        "torch",
+        "opencv-python",
+        "requests",
+        "torchvision",
+        "urllib3",
+        "spacy",
+        "lxml"
+    ],
     package_data={
         'exsclaim': ['figures/config/yolov3_default_master.cfg',
                      'figures/config/yolov3_default_subfig.cfg',
                      'captions/models/characterization.yml',
                      'captions/models/patterns.yml',
                      'captions/models/reference.yml',
-                     'captions/models/rules.yml']
+                     'captions/models/rules.yml',
+                     'tests/data/nature_test.json']
     },
     classifiers=[
         "Development Status :: 1 - Planning",
