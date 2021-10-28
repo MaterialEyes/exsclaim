5da96416fa79c3e59c35de532df06a2688b2a609
Trevor Spreadbury <trevorspreadbury@gmail.com>
2021-10-27 21:44:39 -0500

    Fix ACS encoding by removing 'Accept-Encoding' header

--- exsclaim/journal.py
+++ exsclaim/journal.py
@@ -223,7 +223,6 @@ class JournalFamily():
             A BeautifulSoup parse tree.
         """
         headers = {"Accept":   "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                   "Accept-Encoding": "gzip, deflate, br",
                    "Accept-Language": "en-US,en;q=0.5",
                    "Upgrade-Insecure-Requests":   "1",
                    "User-Agent":  "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0"}
