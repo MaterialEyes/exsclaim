3086369b7d54f1bca8a27ba2a56ca502d8effe17
Trevor Spreadbury <trevorspreadbury@gmail.com>
2021-12-18 18:57:57 -0600

    remove ui code -- moved to https://github.com/MaterialEyes/exsclaim-ui

--- exsclaim/ui/home/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home_view
-
-urlpatterns = [
-    path('', home_view, name="index"),
-]
--- exsclaim/ui/query/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QueryConfig(AppConfig):
-    name = 'exsclaim.ui.query'
